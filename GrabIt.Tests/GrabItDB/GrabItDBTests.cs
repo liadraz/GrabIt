5a08b506ec251d06ad1bd6cd66296e4695b6c15a
liadraz <raz.liad@gmail.com>
2025-03-01 20:12:31 +0200

    Add tests proj

---
+++ GrabIt.Tests/GrabItDB/GrabItDBTests.cs
@@ -0,0 +1,15 @@
+﻿namespace GrabIt.Infrastructure
+{
+    public class GrabItDBTests
+    {
+        [Fact]
+        public void TestConnectionToDB()
+        {
+            GrabItDB db = new GrabItDB();
+
+            Assert.NotNull(db);
+            Assert.True(db.OpenConnection());
+            Assert.True(db.CloseConnection());
+        }
+    }
+}
